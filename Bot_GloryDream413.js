fa219c99e45a02a1def65a7e5e5bf5048b12f9e1
GloryDream413 <putnikdeveloperbg@gmail.com>
2023-04-06 23:59:41 +0200

    Update.

--- Bot_GloryDream413.js
+++ Bot_GloryDream413.js
@@ -18,7 +18,7 @@ async function createPrediction (text) {
       // See https://replicate.com/stability-ai/stable-diffussion/versions
       version:
         '436b051ebd8f68d23e83d22de5e198e0995357afef113768c20f0b6fcef23c8b', //stable-diffussion
-      input: { prompt: "mdjrny-v4 style " + text }
+      input: { prompt: 'mdjrny-v4 style ' + text }
     },
     {
       headers: {
